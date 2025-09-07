4aaeaf760a413cace3e81bcbd4d09f6d467abdf3
/GaiTod✅ <phichitphu2015@gmail.com>
2025-09-08 01:37:05 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h> // Infrared photoelectric switch Sensor E18-D80NK 
 
 const int sensorPin1 = D5;   // เซนเซอร์ขวา
-const int sensorPin2 = D5;
+const int sensorPin2 = D6;
 
 //Led ของเซนเซอร์ ขวา
-int Rsensor1 = D4;    
+int Rsensor1 = D0;    
 int Gsensor1 = D2;
 
-int Rsensor2 = D2;
-int Gsensor2 = D4;
+int Rsensor2 = D7;
+int Gsensor2 = D1;
 
 //กันค่ารวน
 int triggerCount1 = 0;
@@ -19,18 +19,19 @@ void setup() {
   pinMode(sensorPin1, INPUT_PULLUP); // sennor ขวา
   pinMode(sensorPin2, INPUT_PULLUP); // sennorอันแรก
 
-
   pinMode(Rsensor1, OUTPUT);
   pinMode(Gsensor1, OUTPUT);
 
-  //pinMode(ledPin, OUTPUT);
+  // pinMode(Rsensor2, OUTPUT);
+  // pinMode(Gsensor2, OUTPUT);
+
   Serial.begin(115200);
 }
 
 void loop() {
 
-  analogWrite(Rsensor1, 0);
-  analogWrite(Gsensor1, 0);
+  // analogWrite(Rsensor1, 0);
+  // analogWrite(Gsensor1, 0);
 
   bool s1 = (digitalRead(sensorPin1) == LOW);
   bool s2 = (digitalRead(sensorPin2) == LOW);
@@ -39,7 +40,7 @@ void loop() {
   // digitalWrite(led2, s2 ? HIGH : LOW);
 
 
-  if (s1 == HIGH ) {           // เมื่อมีรถมาจอดไฟจะติด
+  if (s1 == LOW ) {           // เมื่อมีรถมาจอดไฟจะติด
 
     triggerCount1++;
 
@@ -56,12 +57,10 @@ void loop() {
     analogWrite(Rsensor1, 0);
     analogWrite(Gsensor1, 255);
     Serial.println("1.Right Senor : CLEAR");
-
   }
 
-  Serial.println("====================");
 
-  if (s2 == HIGH ) {           // เมื่อมีรถมาจอดไฟจะติด
+  if (s2 == LOW ) {           // เมื่อมีรถมาจอดไฟจะติด HIGH
 
     triggerCount2++;
 
@@ -69,6 +68,7 @@ void loop() {
       analogWrite(Rsensor2, 255);
       analogWrite(Gsensor2, 0);
       Serial.println("2.Left Senor : Detected");
+      Serial.println("====================");
     }
   } 
     else {
@@ -77,10 +77,10 @@ void loop() {
     analogWrite(Rsensor2, 0);
     analogWrite(Gsensor2, 255);
     Serial.println("2.Left Senor : CLEAR");
+    Serial.println("====================");
 
   }
 
-  Serial.println("====================");
 
-  delay(1000);
+  delay(500);
 }
